fa3a25a7fc8ec5a402c4f1ce712a06b44963e85b
hroist <111881583+hroist@users.noreply.github.com>
2022-11-09 22:25:53 +0100

    showcase photos added

--- src/sections/Showcase.js
+++ src/sections/Showcase.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import womanSitting from '../assets/images/placeholders/woman-sitting.svg'
-import manSitting from '../assets/images/placeholders/man-sitting.svg'
 import ButtonThemeColor from '../components/ui/ButtonThemeColor'
 import { NavLink } from 'react-router-dom'
 
@@ -8,7 +6,7 @@ const Showcase = () => {
   return (
     <section className="showcase">
       <div className="container">
-        <img src={womanSitting} alt="Silhuette of woman sitting."/>
+        <img src="https://win22imgstorage.blob.core.windows.net/images/showcase-img-1.png" alt="Silhuette of woman sitting."/>
         <div className="showcase-text c-content">
             <h1>SALE UP <br/>To 50% Off</h1>
             <p>Online shopping free home delivery over $100</p>
@@ -16,7 +14,7 @@ const Showcase = () => {
             <ButtonThemeColor buttontext="SHOP NOW" />
             </NavLink>
         </div>
-        <img src={manSitting} alt="Silhuette of man sitting down."/>
+        <img src="https://win22imgstorage.blob.core.windows.net/images/showcase-img-2.png" alt="Silhuette of man sitting down."/>
       </div>
     </section>
   )
